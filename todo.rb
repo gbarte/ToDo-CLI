1813c674acb61f737f17fed2826100b5e45272e1
Gabriele Barteskaite <gbarteskaite1@sheffield.ac.uk>
2018-12-18 22:04:05 +0000

    Stage 2 - Priority

--- todo.rb
+++ todo.rb
@@ -1,24 +1,52 @@
 $todo_list = []
+$priority = 0
 
 def setup
-  puts 'Todo List v0.3'
+  puts 'Todo List v0.21'
 end
 
 def finished()
-  puts "Bye..."
+  puts 'Bye...'
 end
 
 def add_todo(todo)
   if todo == ''
     puts '  :| Nothing to Add - Try \'A todo description\''
   else 
-    $todo_list.push [0, todo]
+    $todo_list.push [$priority, todo]
   end
 end 
 
 def list_todo()
   $todo_list.each.with_index do |todo, index|
       puts "  #{index} (#{todo[0]}) #{todo[1]}"
+  end
+end
+
+def set_priority(number)
+  number = number.to_i
+  if number >=0 && number <= 9
+    $priority = number
+  else
+    puts '  :( Priority must be 0 to 9'
+  end
+end
+
+def increase_priority(index)
+  index = index.to_i
+  if $todo_list[index][0] == 9
+    puts '  :( Priority cannot be higher than 9'
+  else
+    $todo_list[index][0] += 1
+  end
+end
+
+def list_todo_from(number)
+  number = number.to_i
+  $todo_list.each.with_index do |todo, index|
+    if todo[0] >= number
+        puts "  #{index} (#{todo[0]}) #{todo[1]}"
+    end
   end
 end
 
@@ -33,8 +61,14 @@ def parse_command(line)
       add_todo(stripped)
     when 'L'
       list_todo()
+    when 'P'
+      set_priority(stripped)
+    when '+'
+      increase_priority(stripped)
+    when '='
+      list_todo_from(stripped)
     else
-      puts 'Commands: Q-uit A-dd L-ist H-elp'
+      puts 'Commands: Q-uit, A-dd. L-ist, H-elp, P-riority'
     end
   end
 end
@@ -44,9 +78,11 @@ def main_loop()
   setup()
   finished = false
   while !finished
-    print ":) "
+    print ':)'
     line = gets.chomp
-    if line == 'Q'
+    if line == 'P'
+        puts "  Priority is currently #{$priority}"
+    elsif line == 'Q'
       finished = true
       finished()
     else
